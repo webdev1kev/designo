eef78641e92bc91e522b1004ffb161f629fda107
Kevin Davis <kevindavis@Kevs-MacBook-Pro.local>
2025-05-02 22:45:06 -0500

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,7 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li className="tracking-[-.01em]">Deployed with Digital Ocean!</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
